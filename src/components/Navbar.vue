c902abae7b57236da316c0e9215af62dda33d3e2
hanjituan <1048416203@qq.com>
2025-04-21 23:32:35 +0800

    feat: 123

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <router-link to="/" class="nav-link">首页</router-link>
+    <router-link to="/about" class="nav-link">关于</router-link>
+    <router-link to="/contact" class="nav-link">联系我们</router-link>
+  </nav>
+</template>
+
+<script setup>
+// 使用 setup 语法糖，这里不需要额外的逻辑
+</script>
+
+<style scoped>
+.nav-link {
+  margin: 0 10px;
+  text-decoration: none;
+}
+
+.router-link-active {
+  color: #42b983;
+}
+</style>
